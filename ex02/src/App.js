88633ddd15bf3d62e00d0a55d75118a77565290d
keenchy <h.kenan81@hotmail.com>
2021-03-30 18:43:39 +0200

    Using the State Object - React

---
+++ ex02/src/App.js
@@ -0,0 +1,14 @@
+import Plant from './Plant';
+import './App.css';
+
+function App() {
+    return ( <
+        div className = "App" >
+        <
+        Plant / >
+        <
+        /div>
+    );
+}
+
+export default App;
