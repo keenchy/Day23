22e31d42c77eb7027fdd92bd3195cde3dc02e774
keenchy <h.kenan81@hotmail.com>
2021-03-30 23:28:54 +0200

    Forms - React

---
+++ ex05/src/App.js
@@ -0,0 +1,14 @@
+import SimpleForm from './SimpleForm';
+import './App.css';
+
+function App() {
+    return ( <
+        div className = "App" >
+        <
+        SimpleForm / >
+        <
+        /div>
+    );
+}
+
+export default App;
